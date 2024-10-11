4fa9ef95eb28586c9a5c11944a427e07b83b1921
Guilherme Soares <gui.acs26@gmail.com>
2024-10-11 14:30:39 +0100

    New stories mobility in the index; +1 story

--- scripts/storiesInfo.js
+++ scripts/storiesInfo.js
@@ -1,15 +1,9 @@
 const allStories = [
     {
-        name: "Love Among Ancient Trees",
-        imgLink: "stories/story1/story1.jpg",
-        storyLink: "stories/story1/story1.html",
-        storyDescription: "An adventurous journey leads to the discovery of a lost city hidden deep within the mountains, where time stands still and secrets of the past unfold."
-    },
-    {
-        name: "Whispers in the Wind",
-        imgLink: "stories/story2/story2.jpg",
-        storyLink: "stories/story2/story2.html",
-        storyDescription: "In a remote village, strange whispers in the wind guide a young girl on a quest to uncover a forgotten prophecy that holds the key to saving her people."
+        name: "Don't handle your love to a random man",
+        imgLink: "stories/story3/story3.png",
+        storyLink: "stories/story3/story3.html",
+        storyDescription: "Sometimes love is not ephemeral, sometimes you can be in love with the idea of a person for more than a life and hope to see a glimpse of them under your final breath."
     },
     {
         name: "Love Among Ancient Trees",
@@ -22,18 +16,6 @@ const allStories = [
         imgLink: "stories/story2/story2.jpg",
         storyLink: "stories/story2/story2.html",
         storyDescription: "In a remote village, strange whispers in the wind guide a young girl on a quest to uncover a forgotten prophecy that holds the key to saving her people."
-    },
-    {
-        name: "Love Among Ancient Trees",
-        imgLink: "stories/story1/story1.jpg",
-        storyLink: "stories/story1/story1.html",
-        storyDescription: "An adventurous journey leads to the discovery of a lost city hidden deep within the mountains, where time stands still and secrets of the past unfold."
-    },
-    {
-        name: "Whispers in the Wind",
-        imgLink: "stories/story2/story2.jpg",
-        storyLink: "stories/story2/story2.html",
-        storyDescription: "In a remote village, strange whispers in the wind guide a young girl on a quest to uncover a forgotten prophecy that holds the key to saving her people."
-    },
+    }
 
 ]
